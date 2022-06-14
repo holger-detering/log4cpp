5b572a0b46c69e460f1c46689d33fe1ce74d0e27
Holger Detering <github@detering-springhoe.de>
2022-06-14 17:37:46 +0200

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,40 @@
+from conans import ConanFile, CMake, tools
+
+class Log4cppConan(ConanFile):
+    name = "log4cpp"
+    version = "1.1.3"
+    license = "LGPL-2.1-or-later"
+    author = "Bastiaan Bakker <bastiaan.bakker@lifeline.nl>"
+    url = "https://sourceforge.net/projects/log4cpp/"
+    description = """\
+A library of C++ classes for flexible logging to files (rolling), syslog, IDSA
+and other destinations. It is modeled after the Log for Java library
+(http://www.log4j.org), staying as close to their API as is reasonable."""
+    topics = ("c++", "logging")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": True}
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://git.code.sf.net/p/log4cpp/codegit log4cpp")
+        # self.run("git clone https://github.com/orocos-toolchain/log4cpp.git")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="log4cpp")
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["log4cpp"]
+#        if self.settings.os == "Linux":
+#            self.cpp_info.libs.append("pthread")
+
