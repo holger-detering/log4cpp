87780aca18aea66c722948aefa9bbe63cca96fcc
holger-detering <github@detering-springhoe.de>
2022-06-15 17:31:22 +0200

    patch CMakeLists.txt
    - add conandata.yml
    - add patchfiles:
      + add minimum required cmake version
      + exclude Makefile and .cvsignore from build
      + add version to library
    - patch during source step

--- conanfile.py
+++ conanfile.py
@@ -1,44 +1,47 @@
 from conans import ConanFile, CMake, tools
 
 class Log4cppConan(ConanFile):
-    name = "log4cpp"
-    version = "1.1.3"
-    license = "LGPL-2.1-or-later"
-    author = "Bastiaan Bakker <bastiaan.bakker@lifeline.nl>"
-    url = "https://sourceforge.net/projects/log4cpp/"
-    description = """\
+  name = "log4cpp"
+  version = "1.1.3"
+  license = "LGPL-2.1-or-later"
+  author = "Bastiaan Bakker <bastiaan.bakker@lifeline.nl>"
+  url = "https://sourceforge.net/projects/log4cpp/"
+  description = """\
 A library of C++ classes for flexible logging to files (rolling), syslog, IDSA
 and other destinations. It is modeled after the Log for Java library
 (http://www.log4j.org), staying as close to their API as is reasonable."""
-    topics = ("c++", "logging")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": True}
-    generators = "cmake"
+  topics = ("c++", "logging")
+  settings = "os", "compiler", "build_type", "arch"
+  options = {"shared": [True, False]}
+  default_options = {"shared": True}
+  generators = "cmake"
+  exports_sources = "patches/*.patch"
 
-    def source(self):
-        self.run("git clone https://git.code.sf.net/p/log4cpp/codegit log4cpp")
+  def source(self):
+    self.run("git clone https://git.code.sf.net/p/log4cpp/codegit log4cpp")
+    for patch in self.conan_data.get("patches", {}).get(self.version, []):
+      tools.patch(**patch)
 
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="log4cpp")
-        return cmake
+  def _configure_cmake(self):
+    cmake = CMake(self)
+    cmake.configure(source_folder="log4cpp")
+    return cmake
 
-    def build_requirements(self):
-        if self.settings.os == "Linux":
-            self.tool_requires("libtool/[>=2.4.6]@")
+  def build_requirements(self):
+    if self.settings.os == "Linux":
+      self.tool_requires("libtool/[>=2.4.6]@")
 
-    def build(self):
-        self.run("cd log4cpp && ./autogen.sh && ./configure");
-        cmake = self._configure_cmake()
-        cmake.build()
+  def build(self):
+    self.run("cd log4cpp && ./autogen.sh && ./configure");
+    cmake = self._configure_cmake()
+    cmake.build()
 
-    def package(self):
-        cmake = self._configure_cmake()
-        cmake.install()
+  def package(self):
+    cmake = self._configure_cmake()
+    cmake.install()
 
-    def package_info(self):
-        self.cpp_info.libs = ["log4cpp"]
+  def package_info(self):
+    self.cpp_info.libs = ["log4cpp"]
 #        if self.settings.os == "Linux":
 #            self.cpp_info.libs.append("pthread")
 
