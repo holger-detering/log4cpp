6ea16639d6a7d4b4d559c3669cfaf401c6d05de7
holger-detering <github@detering-springhoe.de>
2022-06-29 19:49:43 +0200

    clean up test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,7 +2,6 @@ import os
 
 from conans import ConanFile, CMake, tools
 
-
 class Log4cppTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
@@ -10,15 +9,8 @@ class Log4cppTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
-        # in "test_package"
         cmake.configure()
         cmake.build()
-
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
-        self.copy('*.so*', dst='bin', src='lib')
 
     def test(self):
         if not tools.cross_building(self):
