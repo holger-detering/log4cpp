26319b0db1bece953390af3a449abe95033499a5
holger-detering <github@detering-springhoe.de>
2023-01-26 18:10:07 +0100

    cleanup conanfile
    - move checksums to conandata.yml
    - remove unused options

--- conanfile.py
+++ conanfile.py
@@ -13,17 +13,15 @@ and other destinations. It is modeled after the Log for Java library
 (http://www.log4j.org), staying as close to their API as is reasonable."""
   topics = ("c++", "logging")
   settings = "os", "compiler", "build_type", "arch"
-  options = {"shared": [True, False]}
-  default_options = {"shared": True}
   generators = "cmake"
   exports_sources = "patches/*.patch"
   tool_requires = "libtool/[>=2.4.6]"
 
   def source(self):
     url = f"https://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-{self.version}.tar.gz"
-    tools.get(url, sha1="74f0fea7931dc1bc4e5cd34a6318cd2a51322041",
-        md5="b9e2cee932da987212f2c74b767b4d8b", verify=False)
-    for patch in self.conan_data.get("patches", {}).get(self.version, []):
+    checksums = self.conan_data["checksums"][self.version][0]
+    tools.get(url, sha1=checksums["sha1"], md5=checksums["md5"], verify=False)
+    for patch in self.conan_data["patches"][self.version]:
       tools.patch(**patch)
 
   def _configure_cmake(self):
@@ -47,4 +45,3 @@ and other destinations. It is modeled after the Log for Java library
       self.cpp_info.libs = ["log4cpp"]
     if self.settings.os == "Linux":
       self.cpp_info.libs.append("pthread")
-
