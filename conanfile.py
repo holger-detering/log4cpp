e0954aa1b537ad92125ddba508fa9059c793c9e1
holger-detering <github@detering-springhoe.de>
2023-01-26 21:44:36 +0100

    prefer already downloaded tarball over http get

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+import os.path
 
 class Log4cppConan(ConanFile):
   name = "log4cpp"
@@ -14,13 +15,23 @@ and other destinations. It is modeled after the Log for Java library
   topics = ("c++", "logging")
   settings = "os", "compiler", "build_type", "arch"
   generators = "cmake"
-  exports_sources = "patches/*.patch"
+  exports_sources = "patches/*.patch", "sources/*.tar.gz"
   tool_requires = "libtool/[>=2.4.6]"
 
-  def source(self):
-    url = f"https://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-{self.version}.tar.gz"
+  def _fetch_sources(self):
+    tarball_name = f"log4cpp-{self.version}.tar.gz"
+    url = "https://downloads.sourceforge.net/project/log4cpp/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/" + tarball_name
+    source_path = "sources/" + tarball_name
     checksums = self.conan_data["checksums"][self.version][0]
-    tools.get(url, sha1=checksums["sha1"], md5=checksums["md5"], verify=False)
+    if os.path.isfile(source_path):
+      tools.check_sha1(source_path, checksums["sha1"])
+      tools.check_md5(source_path, checksums["md5"])
+      tools.unzip(source_path)
+    else:
+      tools.get(url, sha1=checksums["sha1"], md5=checksums["md5"], verify=False)
+
+  def source(self):
+    self._fetch_sources()
     for patch in self.conan_data["patches"][self.version]:
       tools.patch(**patch)
 
